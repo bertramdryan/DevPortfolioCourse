84bff838e53a7f4460c5523e6bb79fff5133c28d
Ryan Bertram <bertramdryan@gmail.com>
2021-12-23 19:54:17 -0500

    updated apiendpoints.cs

--- Client/Static/ApiEndpoints.cs
+++ Client/Static/ApiEndpoints.cs
@@ -5,7 +5,7 @@
 #if DEBUG
         internal const string ServerBaseUrl = "https://localhost:5003";
 #else
-        internal const string ServerBaseUrl = "https://appname.azurewebsites.net";
+        internal const string ServerBaseUrl = "https://devportfoliobert.azurewebsites.net";
 #endif
 
         internal readonly static string s_catetories = $"{ServerBaseUrl}/api/categories";
