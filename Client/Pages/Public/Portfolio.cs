7527c738b8c3855024be9ff0420c70e04b3b1bce
Ryan Bertram <bertramdryan@gmail.com>
2021-12-28 14:49:50 -0500

    Expanding the portfolio page (1/2)

---
+++ Client/Pages/Public/Portfolio.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Pages.Public.Portfolio;
+
+public partial class Portfolio : ComponentBase
+{
+    private string _exampleExcerpt =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum ultricies ex, et euismod nibh consectetur non. Morbi tincidunt dui venenatis rutrum elementum. Integer luctus magna ut odio dapibus, non congue erat blandit. Maecenas vitae sollicitudin urna. Maecenas pulvinar nisi id tortor luctus aliquam. Curabitur in est justo. Suspendisse volutpat velit sed sem imperdiet, vitae facilisis risus sodales. Nullam nec volutpat libero. Suspendisse id felis mi.";
+    
+}
