4f19171feabeebeae671981095f04665004d06ff
Ryan Bertram <bertramdryan@gmail.com>
2021-12-29 12:08:58 -0500

    finishing the modal window component.

--- Client/Components/Public/Portfolio/ExamplePortfolioProjectModalWindow.cs
+++ Client/Components/Public/Portfolio/ExamplePortfolioProjectModalWindow.cs
@@ -4,5 +4,25 @@ namespace Client.Components.Public.Portfolio;
 
 public partial class ExamplePortfolioProjectModalWindow : ComponentBase
 {
+    [Parameter] public EventCallback ParentMethodToCallbackOnClickBtnClose { get; set; }
+    private string _modalDisplay = null;
+    private string _modalClass = null;
+    private bool _showBackdrop = false;
+
+    protected override void OnInitialized() => ShowModal();
+
+    private void ShowModal()
+    {
+        _modalDisplay = "block;";
+        _modalClass = "show";
+        _showBackdrop = true;
+    }
     
+    private void CloseModal()
+    {
+        _modalDisplay = "none";
+        _modalClass = string.Empty;
+        _showBackdrop = false;
+        ParentMethodToCallbackOnClickBtnClose.InvokeAsync();
+    }
 }
--- Client/Pages/Public/Portfolio.cs
+++ Client/Pages/Public/Portfolio.cs
@@ -4,6 +4,7 @@ namespace Pages.Public.Portfolio;
 
 public partial class Portfolio : ComponentBase
 {
+    private bool _showingExamplePortfolioProjectModalWindow = false;
     private string _exampleExcerpt =
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum ultricies ex, et euismod nibh consectetur non. Morbi tincidunt dui venenatis rutrum elementum. Integer luctus magna ut odio dapibus, non congue erat blandit. Maecenas vitae sollicitudin urna. Maecenas pulvinar nisi id tortor luctus aliquam. Curabitur in est justo. Suspendisse volutpat velit sed sem imperdiet, vitae facilisis risus sodales. Nullam nec volutpat libero. Suspendisse id felis mi.";
     
