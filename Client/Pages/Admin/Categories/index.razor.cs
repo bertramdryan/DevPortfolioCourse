62520109ee4698949391d1f69974318e6479cc67
Ryan Bertram <bertramdryan@gmail.com>
2022-01-26 13:10:43 -0500

    creating the categories index page

---
+++ Client/Pages/Admin/Categories/index.razor.cs
@@ -0,0 +1,35 @@
+﻿using Client.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Client.Pages.Admin.Categories
+{
+
+    public partial class index : IDisposable
+    {
+        [Inject] InMemoryDatabaseCache InMemoryDatabaseCache { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            InMemoryDatabaseCache.OnCategoriesDataChanged += StateHasChanged;
+
+            if (InMemoryDatabaseCache.Categories == null)
+            {
+                await InMemoryDatabaseCache.GetCategoriesFromDatabaseAndCache();
+            }
+
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                InMemoryDatabaseCache.OnCategoriesDataChanged -= StateHasChanged;
+            }
+        }
+    }
+}
