78408f6a7f6ab1c4a9603e8e4b9dbd5774b30b9a
Ryan Bertram <bertramdryan@gmail.com>
2022-02-25 23:17:42 -0500

    upload image implimented.

--- Client/Pages/Admin/Categories/Create.razor.cs
+++ Client/Pages/Admin/Categories/Create.razor.cs
@@ -3,6 +3,7 @@ using System.Net.Http.Json;
 using Client.Services;
 using Client.Static;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Shared.Models;
 
 namespace Client.Pages.Admin.Categories;
@@ -33,9 +34,56 @@ public partial class Create : ComponentBase
         else
         {
             _attemptToCrateFailed = true;
+        }        
+    }
+
+    private bool _attemptingUploadImage = false;
+    private bool _attemptUploadFailed = false;
+    private string _reasonImageUploadFailed = null;
+    private async Task HandleUploadedImage(InputFileChangeEventArgs e)
+    {
+        _attemptingUploadImage = true;
+
+        if(e.File.ContentType != "image/jpeg" && e.File.ContentType != "image/png")
+        {
+            _reasonImageUploadFailed = "Please only upload JPG, JPEG, or PNG images.";
+            _attemptUploadFailed = true;
+        }
+        else if(e.File.Size >= 31457280) // 30mb
+        {
+            _reasonImageUploadFailed = "Please keep files under 30mb";
+            _attemptUploadFailed = true;
+        }
+        else
+        {
+            IBrowserFile uploadedImageFile = e.File;
+
+            byte[] imageAsByArray = new byte[uploadedImageFile.Size];
+
+            // fills the content of image byte array
+            await uploadedImageFile.OpenReadStream(31457280).ReadAsync(imageAsByArray);
+            string byteString = Convert.ToBase64String(imageAsByArray);
+
+            UploadedImage uploadedImage = new UploadedImage()
+            {
+                NewImageFileExtention = uploadedImageFile.Name.Substring(uploadedImageFile.Name.Length - 4),
+                NewImageBase64Content = byteString,
+                OldImagePath = string.Empty,
+            };
+
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync<UploadedImage>(ApiEndpoints.s_imageUpload, uploadedImage);
+
+            if (response.StatusCode == HttpStatusCode.Created)
+            {
+                _categoryToCreate.ThumbnailImagePath = await response.Content.ReadAsStringAsync();
+            } else
+            {
+                _reasonImageUploadFailed = $"Something when wrong when making a request to the server. Server Code: {response.StatusCode}. Server reason: {response.ReasonPhrase}";
+                _attemptUploadFailed=true;
+            }
         }
 
-        _attemptingToCreate = false;
-        
+        _attemptingUploadImage = false;
+        StateHasChanged();
     }
 }
