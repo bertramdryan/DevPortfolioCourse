bc77e996c62f3659bc60f5fb9fa06230147dcb54
Ryan Bertram <bertramdryan@gmail.com>
2022-03-08 21:16:37 -0500

    Creating some resusable components

---
+++ Client/Components/Shared/Toast.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Client.Components.Shared
+{
+    public partial class Toast : ComponentBase
+    {
+        [Parameter] public bool IsError { get; set; }
+        [Parameter] public string ToastTitle { get; set; }
+        [Parameter] public string ToastBody { get; set; }
+        [Parameter] public EventCallback ParentMethodToCallOnClickBtnClose { get; set; }
+
+        private string _showClass = string.Empty;
+        private string _time = string.Empty;
+
+        protected void OnAfterRenderAsync()
+        {
+            _time = DateTime.Now.ToString("hh:mm:ss tt");
+            _showClass = "show";
+        }
+    }
+}
